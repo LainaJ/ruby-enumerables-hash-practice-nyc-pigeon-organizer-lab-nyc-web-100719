98ea551640d28008719018c81aeaf489d93821ce
Laina Karosic <lainakarosic@gmail.com>
2019-09-18 17:58:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |categories, value|
+    value.each do |attributes, names|
+      names.each do |a_name|
+        if pigeon_list[a_name] == nil
+          pigeon_list[a_name] = {}
+        end
+        if pigeon_list[a_name][categories] == nil
+          pigeon_list[a_name][categories] = []
+        end
+          pigeon_list[a_name][categories].push(attributes.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
