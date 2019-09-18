5e6cddeb3c7172f5c64527657187ce23a4d94316
Laina Karosic <lainakarosic@gmail.com>
2019-09-18 17:40:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
           pigeon_list[a_name] = {}
         end
         if pigeon_list[a_name][categories] == nil
-          pigeon_list[a_name][categories] = {}
+          pigeon_list[a_name][categories] = []
         end
           pigeon_list[a_name][categories].push(attributes.to_s)
       end
